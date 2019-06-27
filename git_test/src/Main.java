7c28c7f335cb92f6e46a9595bdd4521c9c6847bb
sublun <sublun@126.com>
2019-06-27 17:03:17 +0800

    第1

--- git_test/src/Main.java
+++ git_test/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World11122333!");
+        System.out.println("Hello World!");
     }
 }
 //shift+Ctrl+"+号"方法的展开
