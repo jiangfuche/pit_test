a7c16b0e3445537811cabb0433edb304c9ed8ef7
sublun <sublun@126.com>
2019-06-27 20:49:14 +0800

    1

--- git_test/src/Main.java
+++ git_test/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!777777");
-        System.out.println(8888888);
-        System.out.println(123456789);
+        System.out.println("Hello World!7");
+        System.out.println(8);
+        System.out.println(1);
     }
 }
